c4305f6dc65597cdedc54b99b2a245fcef0efae6
Aditinegi97 <aditinegi9756@gmail.com>
2024-11-26 22:32:23 +0530

    Initial commit with basic To-Do List code

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim()) {
+        const li = document.createElement('li');
+        li.textContent = taskInput.value;
+
+        li.onclick = () => {
+            li.remove();
+        };
+
+        taskList.appendChild(li);
+        taskInput.value = '';
+    } else {
+        alert('Please enter a task!');
+    }
+}
